c53b184df06dc1773d51963185c249bd2a37201f
unknown <esunar@wpi.edu>
2024-07-02 10:46:15 -0400

    removed CM3 from cmake

--- stm32/stm32f10X/STM32F10x_StdPeriph_Lib_V3.5.0/CMakeLists/stm32f10x.cmake
+++ stm32/stm32f10X/STM32F10x_StdPeriph_Lib_V3.5.0/CMakeLists/stm32f10x.cmake
@@ -33,8 +33,8 @@ FILE(GLOB COPY_HEADER_FROM_LIB
       ${MCU_SDK_PATH}/Libraries/CMSIS/Core/Template/ARMv8-M/ *.c
       ${MCU_SDK_PATH}/Libraries/STM32F10x_StdPeriph_Driver/inc/*.h
       ${MCU_SDK_PATH}/Project/STM32F10x_StdPeriph_Template/stm32f1xx_hal_conf.h
-      ${MCU_SDK_PATH}/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.c
-	  ${MCU_SDK_PATH}/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/*.h
+     # ${MCU_SDK_PATH}/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.c
+	 # ${MCU_SDK_PATH}/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/*.h
       )
 
 #message(COPY_HEADER_FROM_LIB "******* $CONF_SOURCE is not defined! ********")
