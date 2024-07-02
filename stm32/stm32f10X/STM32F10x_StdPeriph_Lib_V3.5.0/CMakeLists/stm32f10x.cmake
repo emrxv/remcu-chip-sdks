a077b162a72cc34678b174c0ff5ecb65cc6ba225
unknown <esunar@wpi.edu>
2024-07-02 12:43:38 -0400

    include Core_A

--- stm32/stm32f10X/STM32F10x_StdPeriph_Lib_V3.5.0/CMakeLists/stm32f10x.cmake
+++ stm32/stm32f10X/STM32F10x_StdPeriph_Lib_V3.5.0/CMakeLists/stm32f10x.cmake
@@ -35,6 +35,7 @@ FILE(GLOB COPY_HEADER_FROM_LIB
       ${MCU_SDK_PATH}/Project/STM32F10x_StdPeriph_Template/stm32f1xx_hal_conf.h
       ${MCU_SDK_PATH}/Libraries/CMSIS/Device/ST/STM32F1xx/Source/Templates/system_stm32f1xx.c
 	  ${MCU_SDK_PATH}/Libraries/CMSIS/Device/ST/STM32F1xx/Include/*.h
+	  ${MCU_SDK_PATH}/Libraries/CMSIS/Core_A/Include/*.h
 	 # ${MCU_SDK_PATH}/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/*.h
       )
 
