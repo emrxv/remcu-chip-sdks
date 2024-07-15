2d85a5a9e7926e852dfa0060ec82fd1317d33aba
Emre Sunar <134863869+emrxv@users.noreply.github.com>
2024-07-15 13:07:34 -0400

    only core_cm3

--- stm32/stm32f10X/STM32F10x_StdPeriph_Lib_V3.5.0/CMakeLists/stm32f10x.cmake
+++ stm32/stm32f10X/STM32F10x_StdPeriph_Lib_V3.5.0/CMakeLists/stm32f10x.cmake
@@ -29,7 +29,6 @@ file(RENAME "${ALL_INCLUDE_DIR}/defines_${MCU_TYPE}.h"
 
 FILE(GLOB COPY_HEADER_FROM_LIB 
       ${MCU_SDK_PATH}/Libraries/CMSIS/Core/Include/core_cm3.h
-      ${MCU_SDK_PATH}/Libraries/CMSIS/Include/*.h
       ${MCU_SDK_PATH}/Libraries/CMSIS/Core/Template/ARMv8-M/ *.c
       ${MCU_SDK_PATH}/Libraries/STM32F10x_StdPeriph_Driver/inc/*.h
       ${MCU_SDK_PATH}/Libraries/STM32F10x_StdPeriph_Driver/inc/Legacy/*.h
